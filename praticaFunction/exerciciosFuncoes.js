af7beb7b3d1f6b9179e320bdbbe28eda5df19bb3
Rafael Euclydes <rafael.euclydes@gmail.com>
2025-12-04 08:29:09 -0300

    feat(work-development): modify exerciciosFuncoes.js file

--- praticaFunction/exerciciosFuncoes.js
+++ praticaFunction/exerciciosFuncoes.js
@@ -80,3 +80,29 @@ function calculaFrete(distancia) {
 }
 
 calculaFrete(15)
+
+
+// Mensagem pós-processamento
+
+// Imagine que você está criando uma função que simula o processamento de um pedido em um site. Após o processamento, você quer que uma mensagem personalizada seja exibida — mas essa mensagem pode variar dependendo do tipo de cliente: comum, vip ou novo usuário.
+
+// Sua tarefa é criar uma função chamada processarPedido, que receba dois parâmetros:
+
+    // o nome do cliente, e
+    // uma função de callback responsável por exibir uma mensagem após o pedido ser processado.
+function processarPedido(nome, tipoCliente, callback) {
+    console.log(`Processando pedido de ${nome}...`)
+    callback(nome, tipoCliente);
+}
+
+function mensagemPersonalizada(nome, tipo) {
+    if(tipo === "vip") {
+        console.log(`Obrigado pela preferência, ${nome}! Você ganhou frete grátis.`) 
+    } else if (tipo === "novo"){
+        console.log(`Bem-vindo(a), ${nome}! Aproveite um cupom de boas-vindas.`)
+    } else {
+        console.log(`Obrigado pela sua compra, ${nome}!`)
+    }
+}
+
+processarPedido("Rafael", "novo", mensagemPersonalizada);
